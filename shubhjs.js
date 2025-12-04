0e76ef5b66493f0a18f31eadbe3c43089090670b
SHUBHAM DADWAL <112474449+shubhamdadwal1@users.noreply.github.com>
2025-12-04 10:10:53 +0530

    Add files via upload

--- shubhjs.js
+++ shubhjs.js
@@ -12,3 +12,17 @@ buttons.forEach(btn => {
 document.querySelector(".add-btn").onclick = function () {
     alert("Product Added to Cart!");
 };
+
+
+
+// Get existing cart count or set to 0
+let count = localStorage.getItem("cartCount") || 0;
+
+// Update button click event
+document.querySelector(".add-btn").addEventListener("click", function () {
+    count++; // increase count
+    localStorage.setItem("cartCount", count); // update storage
+});
+document.querySelector(".add-btn").addEventListener("click", function() {
+    window.location.href = "index3.html";
+});
